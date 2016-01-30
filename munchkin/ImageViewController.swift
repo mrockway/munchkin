c565c700bd8a2bc306711f4c00be33b8ddd1a306
mrockway <mrockway@gmail.com>
2016-01-29 18:13:44 -0800

    refactor imageviewcontroller

--- munchkin/ImageViewController.swift
+++ munchkin/ImageViewController.swift
@@ -16,13 +16,18 @@ class ImageViewController: UIViewController {
     
     @IBOutlet weak var weeklyImage: UIImageView!
     
-    func findWeek() {
-        let today = NSDate()
+    func getDate() -> NSDate {
         let defaults = NSUserDefaults.standardUserDefaults()
         let dueDate = defaults.objectForKey("DueDate")
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "yyyy/MM/dd"
-        let weeksLeft = String(Int(ceil(dueDate!.timeIntervalSinceDate(today)/604800)))
+        return dueDate as! NSDate
+    }
+    func findWeek() {
+        let dueDate = getDate()
+        let today = NSDate()
+        let weeks = dueDate.timeIntervalSinceDate(today)/604800
+        let weeksLeft = String(Int(ceil(weeks)))
         weeklyImage.image = UIImage(named: "\(weeksLeft)")
     }
     
@@ -33,7 +38,7 @@ class ImageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         findWeek()
-                // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
